9c19703eba69967a5d084d751480cf93c3a46b04
SukaHati <48337427+SukaHati@users.noreply.github.com>
2019-08-05 12:45:00 +0800

    Add files via upload

---
+++ ClickerGame/src/clickergame/ClickerGame.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clickergame;
+
+/**
+ *
+ * @author user
+ */
+public class ClickerGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        mainscreen myscreen = new mainscreen();
+        //myscreen.setjLabelText();
+    }
+    
+}
